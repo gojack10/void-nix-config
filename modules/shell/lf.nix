bc6b427eeda834df723921db8d0aef7c524d1ffe
jack <jack@local>
2026-01-24 11:50:08 -0800

    feat(shell): enhance lf with nvim editor and mime-type open handler
    CHANGES:
    
    - Set nvim as default editor for lf
    - Add custom open cmd using mime-type detection
    - Open JSON in browser, text files in nvim, rest via xdg-open
    - Add esc binding to unselect

--- modules/shell/lf.nix
+++ modules/shell/lf.nix
@@ -7,12 +7,30 @@
     set icons true
     set ignorecase true
 
+    # Set nvim as default editor
+    set editor nvim
+
     # Delete with confirmation (works with visual selection)
     cmd delete ''${{
       set -f
       printf "Delete ''$fx? [y/N] "
       read ans
       [ "''$ans" = "y" ] && rm -rf ''$fx
+    }}
+
+    # Custom open command - uses mime-type to decide handler
+    cmd open ''${{
+      case $(file --mime-type -Lb $f) in
+        # JSON files open in browser
+        application/json)
+          xdg-open $f ;;
+        # Text-based files open in nvim
+        text/*|application/javascript|application/xml|application/x-shellscript|application/x-perl|application/x-ruby|application/x-python|application/x-php|inode/x-empty)
+          nvim $f ;;
+        # Everything else (images, binaries, PDFs, etc.) use system default
+        *)
+          xdg-open $f ;;
+      esac
     }}
 
     # Bindings
@@ -22,12 +40,8 @@
     # Clear selection after paste
     map p :paste; clear
 
-    # Open files with xdg-open
-    cmd open ''${{
-      xdg-open "''$f" >/dev/null 2>&1 &
-    }}
-
     map <enter> open
     map o open
+    map <esc> unselect
   '';
 }
