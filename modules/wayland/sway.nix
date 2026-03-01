b0a3079f38ec7b0cd5a8157cad36b5014d551eb9
jack <gojack10@gmail.com>
2026-03-01 15:08:34 -0800

    fix(sway): reduce touchpad pointer acceleration
    Lower pointer_accel from 0.5 to 0.15 for better control.

--- modules/wayland/sway.nix
+++ modules/wayland/sway.nix
@@ -175,7 +175,7 @@
           tap = "enabled";
           natural_scroll = "disabled";
           accel_profile = "adaptive";
-          pointer_accel = "0.5";
+          pointer_accel = "0.15";
         };
         "type:keyboard" = {
           repeat_delay = "300";
