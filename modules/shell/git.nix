3c972ec398f4a694b858bebe96918f01c4f317c4
jack <gojack10@gmail.com>
2026-03-24 08:06:08 -0700

    feat(git): add default branch and gh credential helper
    CHANGES:
    
    - Set init.defaultBranch to main
    - Configure gh as credential helper for github.com

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -4,6 +4,8 @@
   programs.git = {
     enable = true;
     settings.user.name = "jack";
-    settings.user.email = "gojack10@gmail.com";  
+    settings.user.email = "gojack10@gmail.com";
+    settings.init.defaultBranch = "main";
+    settings.credential."https://github.com".helper = "!/usr/bin/gh auth git-credential";
   };
 }
