5af46f81ed3ee50c2451babf8056c5be358e64e2
jack <gojack10@gmail.com>
2026-05-08 19:45:55 -0700

    feat(packages): fetch huggingface-hub dynamically from pypi
    CHANGES:
    
    - Override huggingface-hub to pull latest version from PyPI JSON
    - Fetch source tarball at eval time instead of using nixpkgs pin

--- modules/packages/common.nix
+++ modules/packages/common.nix
@@ -1,5 +1,11 @@
 { config, pkgs, ... }:
 
+let
+  pypi-json = builtins.fetchurl {
+    url = "https://pypi.org/pypi/huggingface_hub/json";
+  };
+in
+
 {
   home.packages = with pkgs; [
     # Terminal & Shell
@@ -9,7 +15,14 @@
 
     # Dev tools
     fastfetch
-    python3Packages.huggingface-hub
+    (python3Packages.huggingface-hub.overrideAttrs (_: let
+      pypi = builtins.fromJSON (builtins.readFile pypi-json);
+    in rec {
+      version = pypi.info.version;
+      src = builtins.fetchurl {
+        url = "https://files.pythonhosted.org/packages/source/h/huggingface_hub/huggingface_hub-${version}.tar.gz";
+      };
+    }))
     htop
     neovim
     ripgrep
